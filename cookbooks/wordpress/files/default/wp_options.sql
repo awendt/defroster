bb846d9fd585b4b821aa723c7bef0607dea792bb
André Wendt <awendt@freeshell.org>
2011-11-20 21:52:33 +0100

    Initial commit

---
+++ cookbooks/wordpress/files/default/wp_options.sql
@@ -0,0 +1,2 @@
+UPDATE `wp_options` SET `option_value`='http://localhost:8080' WHERE `option_name`='siteurl';
+UPDATE `wp_options` SET `option_value`='http://localhost:8080' WHERE `option_name`='home';
---
+++ cookbooks/wordpress/recipes/default.rb
@@ -0,0 +1,72 @@
+execute "apt-get update"
+package "apache2"
+package "mysql-server"
+package "php5"
+package "php5-mysql"
+package "vim"
+
+file "/var/www/index.html" do
+  action :delete
+end
+
+cookbook_file "/tmp/backwpup.tar.gz" do
+  source "backwpup.tar.gz"
+  action :create_if_missing
+end
+
+execute "extract WP archive" do
+  command "tar -C /var/www -xzf /tmp/backwpup.tar.gz"
+  creates "/var/www/.htaccess"
+end
+
+execute "a2enmod rewrite"
+execute "a2enmod headers"
+execute "a2enmod filter"
+
+cookbook_file "/etc/apache2/sites-available/wordpress" do
+  source "wordpress.conf"
+  mode "0644"
+  action :create_if_missing
+end
+
+execute "a2dissite default"
+execute "a2ensite wordpress"
+
+additional_lines = <<-EOS
+  define('WP_HOME','http://localhost:8080');
+  define('WP_SITEURL','http://localhost:8080');
+EOS
+
+bash "configure Wordpress for local" do
+  code <<-EOS
+    cd /var/www
+    sed -i -e "s/^define('DB_NAME'.*/define('DB_NAME', 'backwpup_vagrant');/" wp-config.php
+    sed -i -e "s/^define('DB_USER'.*/define('DB_USER', 'root');/" wp-config.php
+    sed -i -e "s/^define('DB_PASSWORD'.*/define('DB_PASSWORD', '');/" wp-config.php
+    sed -i -e "s/^define('DB_HOST'.*/define('DB_HOST', 'localhost');/" wp-config.php
+  EOS
+end
+
+execute "correct file ownership" do
+  command "chown -R root:www-data /var/www"
+end
+
+execute "create database" do
+  command "mysqladmin -u root create backwpup_vagrant"
+  creates "/var/lib/mysql/backwpup_vagrant"
+end
+
+bash "restore dump" do
+  code "mysql -u root backwpup_vagrant < /var/www/*.sql"
+  only_if { %x(mysql -u root -e 'show tables' backwpup_vagrant).empty? }
+end
+
+cookbook_file "/tmp/wp_options.sql" do
+  source "wp_options.sql"
+  action :create_if_missing
+end
+
+execute "tweak options to match this VM" do
+  command "mysql -u root backwpup_vagrant < /tmp/wp_options.sql > /var/log/wp_options.log"
+  creates "/var/log/wp_options.log"
+end
